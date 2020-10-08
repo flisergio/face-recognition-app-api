e31002bbf8d33122cb3ca80ae7f014010236e497
Sergio <sergey.dko@gmail.com>
2020-10-08 17:46:00 +0200

    Added source files

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '4bae1c4584074201a4e22836aa649283'
+});
+
+const handleAPICall = (req, res) => {
+    app.models
+    .predict('d02b4508df58432fbb84e800597b8959', req.body.input)
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => escape.status(400).json('Unable to work with API!'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+
+    db('users').where('id','=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0])
+    })
+    .catch(err => res.status(400).json('Unable to get entries!'))
+}
+
+module.exports = {
+    handleImage: handleImage,
+    handleAPICall: handleAPICall
+}
---
+++ controllers/signin.js
@@ -0,0 +1,37 @@
+const handleSignIn = (db, bcrypt) => (req, res) => {
+    // bcrypt.compare("apples", "$2b$10$RaYj.wzG/NxqUhwj1QWqce19wpGTnxIboKs.77X7DMmAZlOwW8AeC", function(err, result) {
+    //     console.log('first guess', result)
+    // });
+
+    // bcrypt.compare("veggies", "$2b$10$RaYj.wzG/NxqUhwj1QWqce19wpGTnxIboKs.77X7DMmAZlOwW8AeC", function(err, result) {
+    //     console.log('second guess', result)
+    // });
+
+    const { email, password } = req.body;
+   
+    if (!email || !password) {
+        return res.status(400).json('Incorrect form submission!');
+    }
+
+    db.select('email', 'hash').from('login')
+    .where('email', '=', email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(password, data[0].hash);
+
+        if (isValid) {
+            return db.select('*').from('users')
+                .where('email', '=', email)
+                .then(user => {
+                    res.json(user[0])
+                })
+                .catch(err => res.status(400).json('Unable to get user!'));
+        } else {
+            res.status(400).json('Wrong credentials!');
+        }
+    })
+    .catch(err => res.status(400).json('Wrong credentials!'));
+}
+
+module.exports = {
+    handleSignIn: handleSignIn
+}
